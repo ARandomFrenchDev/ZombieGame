1d9d0d4fdd3c46d934423ae5a1758744573e6f80
Rayan Bassetti <r_bassetti@medexo.com>
2023-08-27 11:42:45 +0200

    update: added enemy ai behavior

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,16 +7,51 @@ public class EnemyAI : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
     NavMeshAgent navMeshAgent;
-    // Start is called before the first frame update
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(isProvoked) {
+            EngageTarget();
+        } 
+        else if (distanceToTarget <= chaseRange) {
+            isProvoked = true;
+        }
+        else if (distanceToTarget > chaseRange) {
+            isProvoked = false;
+        }
+
+    }
+
+    private void EngageTarget() {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance) {
+            ChaseTarget();
+        }
+        if(distanceToTarget <= navMeshAgent.stoppingDistance) {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget() {
         navMeshAgent.SetDestination(target.position);
+
+    }
+
+    private void AttackTarget() {
+        Debug.Log("MANGER HUMAIN.");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
