1aeaa7e5a4de80aba884d47823ffdf3346f2bc55
Rayan Bassetti <r_bassetti@medexo.com>
2023-08-30 12:15:59 +0200

    update: added modifications when zooming

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine; 
- 
+using Cinemachine;
+using StarterAssets;
+
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera fpsCamera;
-    [SerializeField] float normalFOV = 30f;
+    [SerializeField] float normalFOV = 60f;
     [SerializeField] float zoomedFOV = 20f;
+    FirstPersonController firstPersonController;
+    [SerializeField] float normalRotationSpeed = 1.0f;
+    [SerializeField] float normalMoveSpeed = 4.0f;
+    [SerializeField] float normalSprintSpeed = 6.0f;
+    [SerializeField] float zoomedRotationSpeed = 0.1f;
+    [SerializeField] float zoomedMoveSpeed = 1f;
+    [SerializeField] float zoomedSprintSpeed = 0.2f;
+
+    void Start() {
+        firstPersonController = GetComponent<FirstPersonController>();
+    }
  
     void Update()
     {
@@ -18,11 +30,17 @@ public class WeaponZoom : MonoBehaviour
         else
         {
             fpsCamera.m_Lens.FieldOfView = normalFOV;
+            firstPersonController.RotationSpeed = normalRotationSpeed;
+            firstPersonController.MoveSpeed = normalMoveSpeed;
+            firstPersonController.SprintSpeed = normalSprintSpeed;
         }
     }
  
     void ZoomView()
     {
         fpsCamera.m_Lens.FieldOfView = zoomedFOV;
+        firstPersonController.RotationSpeed = zoomedRotationSpeed;
+        firstPersonController.MoveSpeed = zoomedMoveSpeed;
+        firstPersonController.SprintSpeed = zoomedSprintSpeed;
     }
 }
