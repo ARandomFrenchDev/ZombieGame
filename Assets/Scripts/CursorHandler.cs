1732ec5a62933e6ade6df8bf8ff46f255376d078
Rayan Bassetti <r_bassetti@medexo.com>
2023-09-07 14:34:19 +0200

    update: paused time when getting into pause menu

--- Assets/Scripts/CursorHandler.cs
+++ Assets/Scripts/CursorHandler.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class CursorHandler : MonoBehaviour
 {
     StarterAssets.StarterAssetsInputs starterAssetsInputs;
+    Weapon weapon;
 
     void Start() {
         starterAssetsInputs = GetComponent<StarterAssets.StarterAssetsInputs>();
+        weapon = FindObjectOfType<Weapon>();
 
     }
 
     public void SetCursorInMenuState(bool state) {
-        Time.timeScale = 0; 
+        Time.timeScale = state ? 1 : 0;
+        weapon.canShoot = state;
         starterAssetsInputs.cursorLocked = state;
         starterAssetsInputs.cursorInputForLook = state;
         starterAssetsInputs.SetCursorState(starterAssetsInputs.cursorLocked);
