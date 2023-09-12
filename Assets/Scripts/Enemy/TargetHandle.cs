6971c0a1518ed7f48ac084965ee6c982c2bfddc2
Rayan Bassetti <r_bassetti@medexo.com>
2023-09-12 10:24:37 +0200

    update: added post processing back + added basic scripting for targets

--- Assets/Scripts/Enemy/TargetHandle.cs
+++ Assets/Scripts/Enemy/TargetHandle.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class TargetHandle : MonoBehaviour
 {
     [SerializeField] float points;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip shotSFX;
     
     public void HandlePoints() {
-        Debug.Log(points);
+        StartCoroutine(CoroutineExecution());
+    }
+
+    IEnumerator CoroutineExecution() {
+        audioSource.PlayOneShot(shotSFX);
+        yield return new WaitUntil(() => !audioSource.isPlaying);
         Destroy(gameObject.transform.parent.gameObject);
+
     }
 
 }
