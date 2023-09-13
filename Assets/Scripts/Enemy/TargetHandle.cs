59079d289d6a16386914f49cb9aa1e127ca75061
Rayan Bassetti <r_bassetti@medexo.com>
2023-09-13 10:51:45 +0200

    update: added radio music + added target disappearance once shot

--- Assets/Scripts/Enemy/TargetHandle.cs
+++ Assets/Scripts/Enemy/TargetHandle.cs
@@ -8,6 +8,7 @@ public class TargetHandle : MonoBehaviour
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip shotSFX;
     [SerializeField] ParticleSystem fireworksEffect;
+    [SerializeField] GameObject components;
     
     public void HandlePoints() {
         StartCoroutine(CoroutineExecution());
@@ -16,6 +17,7 @@ public class TargetHandle : MonoBehaviour
     IEnumerator CoroutineExecution() {
         audioSource.PlayOneShot(shotSFX);
         Instantiate(fireworksEffect, transform.position, transform.rotation);
+        components.SetActive(false);
         yield return new WaitUntil(() => !audioSource.isPlaying);
         Destroy(gameObject.transform.parent.gameObject);
 
